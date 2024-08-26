8d968a69d93c9079891b2f5b849244dde6b38e69
lmstrand <lmstrand@gmail.com>
2024-08-26 15:02:26 +0300

    Add script, configuration and refine description

--- Koha/Plugin/Fi/KohaSuomi/RestrictPatronModification/script.js
+++ Koha/Plugin/Fi/KohaSuomi/RestrictPatronModification/script.js
@@ -1,1 +1,36 @@
 
+/// ALKU ///
+// Virkailijatunnusten muokkausten esto
+$(document).ready(function() {
+    if (window.location.pathname.includes('/cgi-bin/koha/members/memberentry.pl') || window.location.pathname.includes('/cgi-bin/koha/members/members-home.pl')){
+        var superlib = $('span#logged-in-info-full').children().hasClass('is_superlibrarian');
+        var categories = "REPLACE_BY_CONFIG_PARAM_A";
+        categories = categories.trim();
+        categories = categories.split(',');
+        if (window.location.pathname.includes('/cgi-bin/koha/members/memberentry.pl')){
+            var e = document.getElementById("categorycode_entry");
+            var value = e.value;
+            if (!superlib && categories.includes(value)){
+                e.disabled = true;
+                $('input#cardnumber').prop('disabled',true);
+                $('input#surname').prop('disabled',true);
+                $('input#firstname').prop('disabled',true);
+            }
+            if (!superlib){
+                categories.forEach(piilotus);
+                function piilotus(item) {
+                    $('select#categorycode_entry option[value="'+ item +'"]').parent().hide();
+                }
+            }
+        }
+        if (window.location.pathname.includes('/cgi-bin/koha/members/members-home.pl')){
+            if (!superlib){
+                categories.forEach(piilotus);
+                function piilotus(item) {
+                    $('div#new-patron-button ul li a[href*="categorycode=' + item + '"]').parent().hide();
+                }
+            }
+        }
+    }
+});
+/// LOPPU ///
